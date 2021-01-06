9322988360a82314741dc3416259f8bca251d1b8
Mykhailo Sharapov <mikesharapov1@gmail.com>
2021-01-06 13:31:16 +0200

    Add project files.

---
+++ Practise/MyLINQExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    public static class MyLINQExtensions
+    {
+        public static IEnumerable<T> ThenBy<T>(this IOrderedEnumerable<T> orderedCollection, Func<T, bool> expression)
+        {
+            return orderedCollection.ThenBy(expression);
+        }
+
+        public static IEnumerable<T> ThenByDescending<T>(this IOrderedEnumerable<T> orderedCollection, Func<T, bool> expression)
+        {
+            return orderedCollection.ThenByDescending(expression);
+        }
+        );
+        public static IEnumerable<T> Join<T, U, K, V>(this IEnumerable<T> outer, IEnumerable<U> inner, Func<T, K> outerKeySelector, Func<U, K> innerKeySelector, Func<T, U, V> resultSelector)
+        {
+            return outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector);
+        }
+    }
+}
---
+++ Practise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Practise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new List<bool>();
+            Toy.OrderBy(o => o, f).ThenBy(t=>t);
+        }
+    }
+}
