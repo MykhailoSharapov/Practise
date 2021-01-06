fedc87229545c8ce19ecd5aa4589766813a851d8
Mykhailo Sharapov <mikesharapov1@gmail.com>
2021-01-06 13:55:23 +0200

    Services added

---
+++ Practise/Services/FileService.cs
@@ -0,0 +1,89 @@
+﻿// <copyright file="FileService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Practise
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// FileService created for writing messages to file.
+    /// </summary>
+    public class FileService
+    {
+        private const string FileExtension = ".txt";
+        private const string FileNameMask = "hh.mm.ss dd.MM.yyyy";
+        private const int CountSavedLogs = 3;
+        private const string DirectoryPath = "Logs\\";
+        private static readonly FileService InstanceValue = new FileService();
+        private readonly string fileName;
+        private readonly double fileLifetime = 2;
+        private readonly StreamWriter sw;
+
+        static FileService()
+        {
+        }
+
+        private FileService()
+        {
+            this.CheckDirectory();
+            this.CheckFilesInDirect();
+            this.fileName = $"{DateTime.UtcNow.ToString(FileNameMask)}{FileExtension}";
+            this.sw = new StreamWriter($"{DirectoryPath}{this.fileName}", true);
+        }
+
+        /// <summary>
+        /// Gets instance of Logger setting for sigleton pattern release.
+        /// </summary>
+        public static FileService Instance => InstanceValue;
+
+        /// <summary>
+        /// Writing input message in file.
+        /// </summary>
+        /// <param name="text">input text.</param>
+        public void Write(string text)
+        {
+            // File.AppendAllText($"{DirectoryPath}{this.fileName}", text);
+            this.sw.WriteLine(text);
+        }
+
+        private void CheckDirectory()
+        {
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
+        }
+
+        private void CheckFilesInDirect()
+        {
+            var filesPath = Directory.GetFiles(DirectoryPath, $"*{FileExtension}", SearchOption.TopDirectoryOnly);
+            if (filesPath.Length > 0)
+            {
+                var files = new List<FileInfo>();
+
+                foreach (string path in filesPath)
+                {
+                    files.Add(new FileInfo(path));
+                }
+
+                files.Sort(new FilePathDateComparer());
+
+                for (var i = 0; i < files.Count; i++)
+                {
+                    if (DateTime.UtcNow - files[i].CreationTimeUtc > TimeSpan.FromDays(this.fileLifetime) || i >= CountSavedLogs - 1)
+                    {
+                        this.DeleteFile(files[i].FullName);
+                    }
+                }
+            }
+        }
+
+        private void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+    }
+}
---
+++ Practise/Services/Logger.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="Logger.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Practise
+{
+    using System;
+
+    /// <summary>
+    /// Logger class to create log messages.
+    /// </summary>
+    public class Logger : ILogger
+    {
+        private const string TimeFormat = "hh:mm:ss";
+        private static readonly Logger InstanceValue = new Logger();
+        private readonly FileService fileService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class.
+        /// Construcor of logger need initialize fileservice to work with him.
+        /// </summary>
+        private Logger()
+        {
+            this.fileService = FileService.Instance;
+        }
+
+        /// <summary>
+        /// Gets instance of logger to transfer it in plases where it will uses.
+        /// </summary>
+        public static Logger Instance => InstanceValue;
+
+        /// <summary>
+        /// Funk for write log with level error.
+        /// </summary>
+        /// <param name="message">text message.</param>
+        /// <param name="ex">exception or null.</param>
+        public void LogError(string message, Exception ex = null)
+        {
+            this.Log(LogLevel.Error, ex != null ? $"{message}:{ex}" : message);
+        }
+
+        /// <summary>
+        /// Funk for write log with level Info.
+        /// </summary>
+        /// <param name="message">text message.</param>
+        public void LogInfo(string message)
+        {
+            this.Log(LogLevel.Info, message);
+        }
+
+        /// <summary>
+        /// Funk for write log with level warning.
+        /// </summary>
+        /// <param name="message">text message.</param>
+        public void LogWarning(string message)
+        {
+            this.Log(LogLevel.Warning, message);
+        }
+
+        /// <summary>
+        /// universal method for writing all types of messadges.
+        /// </summary>
+        /// <param name="logLevel">log level look in enums.</param>
+        /// <param name="message">text of message.</param>
+        public void Log(LogLevel logLevel, string message)
+        {
+            this.WriteData($"{DateTime.UtcNow.ToString(TimeFormat)}:{logLevel}:{message}");
+        }
+
+        private void WriteData(string data)
+        {
+            this.fileService.Write(data);
+            Console.WriteLine(data);
+        }
+    }
+}
